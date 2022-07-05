b21dddfaaa6f0b1f871438b45ca6e7c007101b55
Michael Huber <michael.huber1981@outlook.com>
2022-07-05 07:31:02 -0500

    Updating logic to include hash routes

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,15 +1,29 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectData } from "../pages/homeSlice";
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
+  const { pathname, hash, key } = useLocation();
   const { name } = useSelector(selectData);
+  const navigate = useNavigate();
 
   React.useEffect(
     function () {
-      window.scrollTo(0, 0);
+      // if not a hash link, scroll to top
+      if (hash === "") {
+        window.scrollTo(0, 0);
+      }
+      // else scroll to id
+      else {
+        const id = hash.replace("#", "");
+        const element = document.getElementById(id);
+        if (element) {
+          element.scrollIntoView();
+        } else {
+          navigate("404", { replace: false });
+        }
+      }
 
       if (pathname === "/") {
         document.title = `${name} | Portfolio`;
@@ -18,7 +32,7 @@ export default function ScrollToTop() {
         document.title = `${name} | All Projects`;
       }
     },
-    [pathname, name]
+    [pathname, name, hash, key, navigate]
   );
 
   return null;
