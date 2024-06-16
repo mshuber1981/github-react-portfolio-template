189527241d9912ce08f5bcc3f380ad48f3dffdc3
mshuber1981 <michael.huber1981@outlook.com>
2024-06-16 09:21:16 -0500

    Removing username

--- src/config.js
+++ src/config.js
@@ -15,7 +15,7 @@ import Logo from "./images/logo.svg";
  **************************************************************
   Add your GitHub username (string - "YourUsername") below.
 */
-export const githubUsername = "mshuber1981";
+export const githubUsername = "Your GitHub username here";
 
 // Navbar Logo image
 export const navLogo = null;
